9872485447cfd008dbe224dd057cb6a0c34bb91f
sabah <mhdsabah42@gmail.com>
2023-08-02 19:33:11 -0400

    initial commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'sequence':-100,
+    'category': 'Hospital',
+    'author': 'Abrus',
+    'summary': 'Hospital Management System',
+    'description': """ Hospital Management System   """,
+    'depends': ['mail','product'],
+    'application':True,
+    'data': [
+        'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
+        'data/patient.tag.csv',
+        'data/sequence_data.xml',
+        'wizard/cancel_appointment_view.xml',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/male_patient_view.xml',
+        'views/appointment_view.xml',
+        'views/patient_tag_view.xml',
+        'views/operation_view.xml',
+        'views/odoo_playground_view.xml',
+        'views/res_config_settings_views.xml',
+
+
+
+    ],
+    'auto_install': False,
+    'licence':'LGPL-3'
+}
